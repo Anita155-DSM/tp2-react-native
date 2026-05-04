3a04c0398bc5b85244cff61fcb485c9c6f6bc224
AnitaDSM-155 <anahiperezdsm@gmail.com>
2026-05-04 11:59:50 -0300

    aplicamos useMemo en la función de filtrado de lista de pokemons, porq devolvemos una lista en este caso, nada más envolvemos con useMemo la constante que ya realizaba esa función

--- frontend/src/components/PokemonList.jsx
+++ frontend/src/components/PokemonList.jsx
@@ -1,14 +1,22 @@
 import PokemonItem from './PokemonItem';
 import { PokemonContext } from '../context/PokemonContext';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 
 function PokemonList({ searchTerm, onEdit }) {
   // consumimos el estado global
   const { state } = useContext(PokemonContext);
-    
-  // filtramos la lista
-  const filteredPokemons = state.pokemons.filter(
-    p => p.name.toLowerCase().includes(searchTerm.toLowerCase()) // Corregido: includes
+  
+  // Referencia (antes, sin memo): aca tenemos la misma constante, nada mas que ahora usando memo la envolvemos
+  // const filteredPokemons = state.pokemons.filter(
+  //   p => p.name.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
+
+  // Memoizamos el filtrado para evitar recalcular en renders sin cambios relevantes.
+  const filteredPokemons = useMemo( //usamos useMemo porq tenemos una lista(lista pokemon)
+    () => state.pokemons.filter(
+      p => p.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ),
+    [state.pokemons, searchTerm]
   );
 
   // evaluamos filteredPokemons
